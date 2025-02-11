8077e5af52b856d5e3d167fb24c6490c03524efd
Johnbyju <johnbyju8@gmail.com>
2025-02-11 12:55:59 +0530

    made changes in the creator application

--- src/components/IFrame.jsx
+++ src/components/IFrame.jsx
@@ -4,7 +4,7 @@ const IFrame = () => {
   return (
     <>
       <iframe
-        src="https://creatorapp.zohopublic.in/john_throughbit/shakman-projects/form-perma/Purchase_Request_Form/3mPFQz366Cf9AwqFn2B03KNP1O7mBgBHvd2BtRx0sfSXgys9kMOsOthKPqJpBQsVVHu6v6tjbrnmre4vfG2K7OEgepUtUnXr0U6D"
+        src="https://creatorapp.zohopublic.in/john_throughbit/shakman-projects/form-embed/Purchase_Request_Form/Y1y9ghxNKbAbMUwYfj9hDae6deGPP2w91QDKUFAjAPs8nO7RVjKshpEkmr7u9xEeB5kwAGsk8gwOD0s0D1j96jktYWShStuyzDO0"
         width="100%"
         height="900px"     
         allowTransparency="true"
--- src/components/Reports.jsx
+++ src/components/Reports.jsx
@@ -4,7 +4,7 @@ const Reports = () => {
   return (
     <>
       <iframe
-         src="https://creatorapp.zohopublic.in/john_throughbit/shakman-projects/report-embed/Request_From_Site/hV71ECPG6BRk7C9FKBHbbpKbpSH6jwfyBzdhHXJE0g9hk7g6qmAng9PEFj0S3pKjTHAQskd5SOVwEhjD24eNCubwtJGFgUuBRQhP"
+         src="https://creatorapp.zohopublic.in/john_throughbit/shakman-projects/report-embed/Request_From_Site/PRMryap1uffHb8MyjJMdd2y6RU3HOeG6XAqUVAwvuB0AUZPsHZbuDk8uGPtveWSV9UsWkugzWyR87phA88VP0EZtAe5gh6BKSFXk"
         width="100%"
         height="900px"
         allowTransparency="true"
